5ff7ff57842c4c678cc678733012140bd55e1e9d
Hacker King <ahmadwaciq03@gmail.com>
2026-04-23 16:50:06 +0430

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 import os
+import time
 
 logo = '''
+[+]
+ |  ██╗░░██╗░█████╗░░█████╗░██╗░░██╗██╗███╗░░██╗░██████╗░░░░░░░████████╗░█████╗░░█████╗░██╗░░░░░
+ |  ██║░░██║██╔══██╗██╔══██╗██║░██╔╝██║████╗░██║██╔════╝░░░░░░░╚══██╔══╝██╔══██╗██╔══██╗██║░░░░░
+ |  ███████║███████║██║░░╚═╝█████═╝░██║██╔██╗██║██║░░██╗░█████╗░░░██║░░░██║░░██║██║░░██║██║░░░░░
+ |  ██╔══██║██╔══██║██║░░██╗██╔═██╗░██║██║╚████║██║░░╚██╗╚════╝░░░██║░░░██║░░██║██║░░██║██║░░░░░
+ |  ██║░░██║██║░░██║╚█████╔╝██║░╚██╗██║██║░╚███║╚██████╔╝░░░░░░░░░██║░░░╚█████╔╝╚█████╔╝███████╗
+ |  ╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝░╚═════╝░░░░░░░░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝ v2.7
+[+] Title    : AllHackingTools - Tool for hacking  -  ⁣ATTENTION! The author of this article is not respo-
+[+] Github   : https://github.com/mishakorzik  -  nsi⁣ble responsible for any consequences of reading it. 
+[+] Coded By : Misha Korzhik (Міша Коржик)  -  All materials are provided for educational purposes only! 
+[+]———————-——-–————————-—-–———-——————-—–————-————-—–———————-——–—————–-——[+]
 ╔════════════════════════════════════╗
 ║        FAROOQ HACK TOOL           ║
 ║        MIX COLOR EDITION          ║
@@ -15,9 +27,22 @@ logo = '''
 [06] Exit Tool
 '''
 
+def clear():
+    os.system("clear")
+
 def banner():
-    # send logo to lolcat for rainbow effect
-    os.system("echo '{}' | lolcat -p 1.6".format(logo))
+    os.system("echo \"{}\" | lolcat -p 1.6".format(logo))
+
+def welcome():
+    print("\033[1;32mWelcome to FAROOQ Tool...\033[0m")
+    time.sleep(1)
+
+def main():
+    clear()
+    welcome()
+    time.sleep(0.5)
+    clear()
+    banner()
 
 if __name__ == "__main__":
-    banner()
+    main()
